7bbe2c4f4d659d69ccd17cfbc58f4a616deb1d84
AW14Saurabh <saurabh.wanivadekar@gmail.com>
2017-01-29 06:23:45 +0530

    Finally C# is done. Hope For the Best.

---
+++ StockManage/LuisDialog.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace StockManage
+{
+    [LuisModel ("c1cf7c1b-edc7-4f4a-a2f5-1e2484e60f5e", "906e574bcd124c8d987afa3c71fd59db")]
+    [Serializable]
+    public class LuisDialog :LuisDialog<object>
+    {
+        [LuisIntent("StockPrice")]
+        public async Task StockIntent(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation STOCK;
+            if(result.TryFindEntity("Equity", out STOCK))
+            {
+                await YahooFinanceAPI.getStockData(STOCK);
+            }
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent ("Pleasentries")]
+        public async Task HiIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Hello");
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("AddNewStock")]
+        public async Task AddNewStockIntent(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation STOCK;
+            if(result.TryFindEntity("Equity", out STOCK))
+            {
+                STOCK.Type = "Destination";
+                await context.PostAsync($"Stock being bought...\n{STOCK.Entity} shares added to profile.");
+            }
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("SellStock")]
+        public async Task SellStockIntent(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation STOCK;
+            if(result.TryFindEntity("Equity", out STOCK))
+            {
+                STOCK.Type = "Destinaiton";
+                await context.PostAsync($"Stock being sold...\n{STOCK.Entity} shares removed from profile.");
+            }
+            context.Wait(MessageReceived);
+        }
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Sorry, I don't understand you.");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ StockManage/YahooFinanceAPI.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace StockManage
+{
+    public class YahooFinanceAPI
+    {
+        public static async Task getStockData(EntityRecommendation Symbol)
+        {
+            try
+            {
+                string URL = $"http://finance.yahoo.com/d/quotes.csv?s={Symbol}&f=nxsac";
+                string CSV;
+                using (WebClient client = new WebClient())
+                {
+                    CSV = await client.DownloadStringTaskAsync(URL).ConfigureAwait(false);
+                }
+                var Line = CSV.Split('\n')[0];
+                var Name = Line.Split(',')[0];
+                var SC = Line.Split(',')[1];
+                var Sym = Line.Split(',')[2];
+                var Price = Line.Split(',')[3];
+                var Change = Line.Split(',')[4];
+                double result=0;
+                if (Price != null && Price.Length >= 0)
+                    double.TryParse(Price, out result);
+                StringBuilder strReplyMessage = new StringBuilder();
+                strReplyMessage.Append($"{Name}");
+                strReplyMessage.Append($"\n");
+                strReplyMessage.Append($"{SC}:{Sym}");
+                strReplyMessage.Append($"\n");
+                strReplyMessage.Append($"{result}");
+                strReplyMessage.Append($"\n\n");
+                strReplyMessage.Append($"\t{Change}");
+            }
+            catch(WebException we)
+            {
+                throw we;
+            }
+        }
+    }
+}
