092d9667faca3680ee35b4f089517864321d2776
AW14Saurabh <saurabh.wanivadekar@gmail.com>
2017-01-29 08:14:29 +0530

    no

--- StockManage/LuisDialog.cs
+++ StockManage/LuisDialog.cs
@@ -1,6 +1,4 @@
-﻿using Chronic.Handlers;
-using Microsoft.Azure;
-using Microsoft.Bot.Builder.Dialogs;
+﻿using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Luis;
 using Microsoft.Bot.Builder.Luis.Models;
 using System;
@@ -8,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace StockManage
 {
-    [LuisModel ("c1cf7c1b-edc7-4f4a-a2f5-1e2484e60f5e", "906e574bcd124c8d987afa3c71fd59db")]
     [Serializable]
+    [LuisModel ("c1cf7c1b-edc7-4f4a-a2f5-1e2484e60f5e", "906e574bcd124c8d987afa3c71fd59db")]
     public class LuisDialog :LuisDialog<object>
     {
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync($"Sorry, I don't understand {result.Query}.");
+            context.Wait(this.MessageReceived);
+        }
         [LuisIntent("StockPrice")]
         public async Task StockIntent(IDialogContext context, LuisResult result)
         {
@@ -20,13 +25,13 @@ namespace StockManage
             {
                 await YahooFinanceAPI.getStockData(STOCK);
             }
-            context.Wait(MessageReceived);
+            context.Wait(this.MessageReceived);
         }
         [LuisIntent ("Pleasentries")]
         public async Task HiIntent(IDialogContext context, LuisResult result)
         {
             await context.PostAsync($"Hello");
-            context.Wait(MessageReceived);
+            context.Wait(this.MessageReceived);
         }
         [LuisIntent("AddNewStock")]
         public async Task AddNewStockIntent(IDialogContext context, LuisResult result)
@@ -37,7 +42,7 @@ namespace StockManage
                 STOCK.Type = "Destination";
                 await context.PostAsync($"Stock being bought...\n{STOCK.Entity} shares added to profile.");
             }
-            context.Wait(MessageReceived);
+            context.Wait(this.MessageReceived);
         }
         [LuisIntent("SellStock")]
         public async Task SellStockIntent(IDialogContext context, LuisResult result)
@@ -48,13 +53,7 @@ namespace StockManage
                 STOCK.Type = "Destinaiton";
                 await context.PostAsync($"Stock being sold...\n{STOCK.Entity} shares removed from profile.");
             }
-            context.Wait(MessageReceived);
-        }
-        [LuisIntent("None")]
-        public async Task NoneIntent(IDialogContext context, LuisResult result)
-        {
-            await context.PostAsync($"Sorry, I don't understand you.");
-            context.Wait(MessageReceived);
+            context.Wait(this.MessageReceived);
         }
     }
 }
